f03b503f15a60d9013a4e62ac68569b677764d2b
Muhammad Irza Arivin Ngibadi <84498109+irzaarivin@users.noreply.github.com>
2023-11-20 11:28:39 +0700

    Update page.jsx

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -8,7 +8,7 @@ const Login = () => {
   const a =  useContext(globalContext)
   console.log(a)
   const [input, setInput] = useState({
-    username: '',
+    email: '',
     password: ''
   })
 
@@ -22,7 +22,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     const data = new FormData()
 
-    data.append('username', input.username);
+    data.append('email', input.email);
     data.append('username', input.password);
 
     const response = await axios.post(process.env.NEXT_PUBLIC_URL, {
@@ -45,13 +45,13 @@ const Login = () => {
         <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
           <div className="max-w-md mx-auto">
             <div>
-              <h1 className="text-2xl font-semibold">Login Form with Floating Labels</h1>
+              <h1 className="text-2xl font-semibold">Login as Cashier</h1>
             </div>
             <div className="divide-y divide-gray-200">
               <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                 <div className="relative">
-                  <input name="username" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="username address" onChange={(e) => handleChange(e)} />
-                  <label htmlFor="username" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">username Address</label>
+                  <input name="email" type="email" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="username address" onChange={(e) => handleChange(e)} />
+                  <label htmlFor="email" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
                 </div>
                 <div className="relative">
                   <input name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" onChange={(e) => handleChange(e)} />
