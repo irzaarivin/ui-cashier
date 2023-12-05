700d5841705a95fa623ee1d28939c7c02c32aa7c
Dimasprasetyo71 <dimas21345@gmail.com>
2023-12-06 00:18:39 +0700

    fix bug

--- src/app/components/cart.jsx
+++ src/app/components/cart.jsx
@@ -1,20 +1,45 @@
 import React, { useState, useRef, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
-import Calculator from './calculator';
 
 const formatCurrency = (amount) => {
-  return new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(amount);
+  const numericAmount = parseFloat(amount);
+
+  if (isNaN(numericAmount) || typeof numericAmount === 'undefined') {
+    return 'Rp0';
+  }
+
+  const formattedAmount = new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(numericAmount);
+
+  const trimmedAmount = formattedAmount.replace(/\.?0+$/, '');
+
+  return trimmedAmount;
+};
+
+const generatePDF = (cartItems, total) => {
+  const doc = new jsPDF();
+  doc.text('Invoice', 10, 10);
+  doc.autoTable({
+    head: [['Nama Barang', 'Jumlah', 'Harga', 'Total']],
+    body: cartItems.map(item => [item.nama_barang, item.quantity, formatCurrency(item.harga_barang), formatCurrency(item.harga_barang * item.quantity)])
+  });
+  doc.text(`Total: ${formatCurrency(total)}`, 10, doc.autoTable.previous.finalY + 10);
+  doc.save('invoice.pdf');
 };
 
 const Cart = ({ cartItems, onCancel, onToggle }) => {
-
   const [cartVisible, setCartVisible] = useState(true);
   const cartRef = useRef(null);
 
-  const total = cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
+  const total = cartItems.reduce((acc, item) => acc + item.harga_barang * item.quantity, 0);
 
   const handleBuy = () => {
-    alert('Terima kasih telah berbelanja!');
+    console.log('Cart Items:', cartItems); // Log the cart items for debugging
+    generatePDF(cartItems, total);
+    toast.success('Terima kasih telah berbelanja!');
   };
 
   const toggleCart = () => {
@@ -43,14 +68,14 @@ const Cart = ({ cartItems, onCancel, onToggle }) => {
         {cartItems.map((item) => (
           <li key={item.id} className="flex justify-between items-center mb-2">
             <p>
-              {item.name} x {item.quantity}
+              {item.nama_barang} x {item.quantity}
             </p>
-            <p>{formatCurrency(item.price * item.quantity)}</p>
+            <p>{formatCurrency(item.harga_barang * item.quantity)}</p>
           </li>
         ))}
       </ul>
       <hr className="my-4" />
-      <p className="text-xl font-semibold">Total: {formatCurrency(total)}</p>
+      <p className="text-xl font-semibold">total beli: {formatCurrency(total)}</p>
       <div className="flex justify-between mt-4">
         <button
           className="text-gray-500 hover:text-gray-700 transition duration-300 ease-in-out transform hover:scale-105"
@@ -62,6 +87,7 @@ const Cart = ({ cartItems, onCancel, onToggle }) => {
           className="bg-green-500 text-white rounded-full p-2 hover:bg-green-600 transition duration-300 ease-in-out transform hover:scale-105"
           onClick={handleBuy}
         >
+          <ToastContainer />
           Beli
         </button>
       </div>
@@ -72,9 +98,6 @@ const Cart = ({ cartItems, onCancel, onToggle }) => {
         >
           Tutup Keranjang
         </button>
-        <div className="">
-          <Calculator />
-        </div>
       </div>
     </div>
   );
--- src/app/components/kasir.jsx
+++ src/app/components/kasir.jsx
@@ -1,54 +1,119 @@
-import React, { useState } from 'react';
-import Image from 'next/image';
+// Kasir.js
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Cart from './Cart';
+import { motion } from 'framer-motion';
 
-const items = [
-  { id: 1, name: 'cilor', image: 'cilor.jpg', price: 5000 },
-  { id: 2, name: 'kebab', image: 'kebab.jpg', price: 5000 },
-  { id: 3, name: 'burger', image: 'burger.jpg', price: 5000 },
-  
-  { id: 4, name: 'teh pucuk', image: 'teh pucuk.jpg', price: 3500 },
-  { id: 5, name: 'Buku tulis', image: 'buku tulis.jpg', price: 6000 },
-  { id: 6, name: 'spaghetti', image: 'spaghetti.jpg', price: 5000 },
+export const formatCurrency = (amount) => {
+  const numericAmount = parseFloat(amount);
 
+  if (isNaN(numericAmount) || typeof numericAmount === 'undefined') {
+    return 'Rp0';
+  }
 
-  
-  { id: 7, name: 'pulpen', image: 'pulpen.png', price: 4000 },
-  { id: 8, name: 'Item 2', image: 'kebab.jpg', price: 5000 },
-];
+  const formattedAmount = new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(numericAmount);
+
+  const trimmedAmount = formattedAmount.replace(/\.?0+$/, '');
+
+  return trimmedAmount;
+};
 
 const Kasir = () => {
-  const imageSize = { width: 600, height: 400 };
-
+  const [items, setItems] = useState([]);
+  const [transactions, setTransactions] = useState([]);
   const [cart, setCart] = useState([]);
-  
   const [selectedItem, setSelectedItem] = useState(null);
-  
   const [isCartVisible, setIsCartVisible] = useState(true);
-  
   const [quantity, setQuantity] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
+  const [confirmationModal, setConfirmationModal] = useState(false);
+  const [scrollPosition, setScrollPosition] = useState(0);
 
+  useEffect(() => {
+    axios.get('http://localhost:3001/barang')
+      .then((response) => {
+        const itemsWithNamaBarang = response.data.map(item => ({ ...item, nama_barang: item.nama_barang }));
+        setItems(itemsWithNamaBarang);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching items:', error);
+        setIsLoading(false);
+      });
+
+    const handleScroll = () => {
+      setScrollPosition(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   const addToCart = () => {
-    const existingItem = cart.find((cartItem) => cartItem.id === selectedItem.id);
-
+    const existingItem = cart.find((cartItem) => cartItem.nama_barang === selectedItem.nama_barang);
+    const updatedQuantity = parseInt(quantity, 10);
+  
+    let updatedCart;
+  
     if (existingItem) {
-      const updatedCart = cart.map((cartItem) =>
-        cartItem.id === selectedItem.id
-          ? { ...cartItem, quantity: cartItem.quantity + quantity }
+      updatedCart = cart.map((cartItem) =>
+        cartItem.nama_barang === selectedItem.nama_barang
+          ? { ...cartItem, quantity: cartItem.quantity + updatedQuantity }
           : cartItem
       );
-
-      setCart(updatedCart);
     } else {
-      setCart([...cart, { ...selectedItem, quantity }]);
+      updatedCart = [...cart, { ...selectedItem, quantity: updatedQuantity }];
     }
-
+  
+    console.log('updatedCart:', updatedCart);
+  
+    setCart(updatedCart);
     setSelectedItem(null);
     setQuantity(1);
   };
+  // const addToCart = () => {
+  //   const existingItem = cart.find((cartItem) => cartItem.id === selectedItem.id);
+  //   const updatedQuantity = parseInt(quantity, 10);
+  
+  //   let updatedCart;
+  
+  //   if (existingItem) {
+  //     updatedCart = cart.map((cartItem) =>
+  //       cartItem.id === selectedItem.id
+  //         ? { ...cartItem, quantity: cartItem.quantity + updatedQuantity }
+  //         : cartItem
+  //     );
+  //   } else {
+  //     updatedCart = [...cart, { ...selectedItem, quantity: updatedQuantity }];
+  //   }
+  
+  //   const updatedStok = selectedItem.stok - updatedQuantity;
+  //   const newStok = Math.max(updatedStok, 0);
+  //   const updatedItems = items.map((item) =>
+  //     item.id === selectedItem.id ? { ...item, stok: newStok } : item
+  //   );
+  
+  //   setCart(updatedCart);
+  //   setSelectedItem(null);
+  //   setQuantity(1);
+  //   setItems(updatedItems);
+  // };
 
-  const formatCurrency = (amount) => {
-    return new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(amount);
+  const handleBuy = () => {
+    setConfirmationModal(true);
+  };
+
+  const confirmTransaction = () => {
+    console.log('Transaction completed:', cart);
+
+    const newTransaction = { id: transactions.length + 1, items: cart };
+    setTransactions([...transactions, newTransaction]);
+
+    setCart([]);
+    setConfirmationModal(false);
   };
 
   const openModal = (item) => {
@@ -63,41 +128,52 @@ const Kasir = () => {
     setIsCartVisible(!isCartVisible);
   };
 
-
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 p-4">
-      {items.map((item) => (
-        <div key={item.id} className="relative bg-white p-4 rounded-lg shadow-md group transition transform hover:scale-105">
-          <div className="relative overflow-hidden rounded-md mb-4 group-hover:opacity-75 transition-opacity duration-300">
-            <Image
-              src={`/image/${item.image}`}
-              alt={item.name}
-              width={imageSize.width}
-              height={imageSize.height}
-              className="object-cover rounded-md mb-4 group-hover:opacity-75 transition-opacity duration-300"
-              layout="responsive"
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0.5 }}
+      transition={{ duration: 0.5 }}
+      className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 p-4"
+    >
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        items.map((item) => (
+          <motion.div
+            key={item.id}
+            className="mb-8"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            <img
+              src={item.gambar_barang}
+              alt={item.nama_barang}
+              className="object-cover rounded-md mb-4"
+              width={300}
+              height={300}
               onClick={() => openModal(item)}
             />
-          </div>
-          <div className="text-center">
-            <p className="text-xl font-semibold mb-2">{item.name}</p>
-          <p className="text-gray-700">Harga: {formatCurrency(item.price)}</p>
-          </div>
-          <div className="flex justify-center items-center mt-10">
-            <button
-              className="bg-green-500 text-white rounded-full p-2 hover:bg-green-600 transition duration-300 ease-in-out transform hover:scale-105"
-              onClick={() => openModal(item)}
-            >
-              Tambah ke Keranjang
-            </button>
-          </div>
-        </div>
-      ))}
+
+            <div className="text-center">
+              <p className="text-xl font-semibold mb-2">{item.nama_barang}</p>
+              <p className="text-gray-700">Harga: {formatCurrency(item.harga_barang)}</p>
+              <p className='text-white'>{item.stok}</p>
+            </div>
+          </motion.div>
+        ))
+      )}
       {selectedItem && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -50 }}
+          transition={{ duration: 0.5 }}
+          className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"
+        >
           <div className="bg-white p-8 rounded-lg text-center">
-            <h2 className="text-2xl font-semibold mb-4">{selectedItem.name}</h2>
-            <p className="text-gray-700 mb-4">Harga: {formatCurrency(selectedItem.price)}</p>
+            <h2 className="text-2xl font-semibold mb-4">{selectedItem.nama_barang}</h2>
+            <p className="text-gray-700 mb-4">Harga: {formatCurrency(selectedItem.harga_barang)}</p>
             <label className="block mb-2">Jumlah:</label>
             <input
               type="number"
@@ -107,40 +183,103 @@ const Kasir = () => {
               className="w-full border p-2 rounded-md mb-4"
             />
             <div className="flex justify-center mb-4">
-              <button
-                aria-label='addtocart'
+              <motion.button
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
                 className="bg-green-500 text-white rounded-full p-2 hover:bg-green-600 mr-4 transition duration-300 ease-in-out transform hover:scale-105"
                 onClick={addToCart}
               >
                 Tambah ke Keranjang
-              </button>
-              <button
-                aria-label="Tambah ke Keranjang"
+              </motion.button>
+              <motion.button
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
                 className="text-gray-500 hover:text-gray-700 transition duration-300 ease-in-out transform hover:scale-105"
                 onClick={closeModal}
               >
                 Batal
-              </button>
-             
+              </motion.button>
             </div>
           </div>
-        </div>
+        </motion.div>
       )}
       {isCartVisible && (
-        <Cart cartItems={cart} onCancel={() => setCart([])} onToggle={toggleCart} />
+        <Cart cartItems={cart} onCancel={() => setCart([])} onToggle={toggleCart} formatCurrency={formatCurrency} />
       )}
       {!isCartVisible && (
         <div className="fixed bottom-4 right-4">
-          <button
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
             className="bg-green-500 text-white rounded-full p-2 hover:bg-green-600 transition duration-300 ease-in-out transform hover:scale-105"
             onClick={toggleCart}
           >
             Buka Keranjang
-          </button>
+          </motion.button>
         </div>
       )}
-      
-    </div>
+      {confirmationModal && (
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -50 }}
+          transition={{ duration: 0.5 }}
+          className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"
+        >
+          <div className="bg-white p-8 rounded-lg text-center">
+            <h2 className="text-2xl font-semibold mb-4">Konfirmasi Transaksi</h2>
+            <p className="text-gray-700 mb-4">Apakah Anda yakin ingin menyelesaikan transaksi?</p>
+            <div className="flex justify-center mb-4">
+              <motion.button
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                className="bg-blue-500 text-white rounded-full p-2 hover:bg-blue-600 transition duration-300 ease-in-out transform hover:scale-105"
+                onClick={confirmTransaction}
+              >
+                Ya
+              </motion.button>
+              <motion.button
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                className="text-gray-500 hover:text-gray-700 transition duration-300 ease-in-out transform hover:scale-105"
+                onClick={() => setConfirmationModal(false)}
+              >
+                Tidak
+              </motion.button>
+            </div>
+          </div>
+        </motion.div>
+      )}
+      {transactions.length > 0 && (
+        <div>
+          <h2 className="text-xl font-semibold mb-4">Data Transaksi</h2>
+          <table className="table-auto">
+            <thead>
+              <tr>
+                <th>ID Transaksi</th>
+                <th>Items</th>
+              </tr>
+            </thead>
+            <tbody>
+              {transactions.map((transaction) => (
+                <tr key={transaction.id}>
+                  <td>{transaction.id}</td>
+                  <td>
+                    <ul>
+                      {transaction.items.map((item) => (
+                        <li key={item.id}>
+                          {item.nama_barang} x {item.quantity}
+                        </li>
+                      ))}
+                    </ul>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </motion.div>
   );
 };
 
